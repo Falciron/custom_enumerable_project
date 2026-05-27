c8ebf1a8e831111611d38a9d32809a0631252d6d
Falciron <2159742+Falciron@users.noreply.github.com>
2026-05-27 09:13:43 -0700

    Implement Enumerable.my_each_with_index
    I'm starting to feel kind of selfish here. My this, my that. Why not
    our_each_with_index?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,15 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index = 0
+    self.my_each do |element|
+      yield element,index
+      index += 1
+    end
+    return self
+  end
 end
 
 # You will first have to define my_each
