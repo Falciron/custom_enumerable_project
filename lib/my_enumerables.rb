e6b12a86de1a38f885763ad94a22cdd33c4cc6a4
Falciron <2159742+Falciron@users.noreply.github.com>
2026-05-27 12:04:14 -0700

    Implement my_any?, my_all?, and my_none?
    Normally I'd implement these one by one, but this time, I gave it my_all

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -16,10 +16,32 @@ module Enumerable
 
     # Enumerable.select always returns an array, regardless of the class of self.
     selected_array = []
-    self.my_each do |element|
-      selected_array << element if yield element
-    end
+    self.my_each { |element| selected_array << element if yield element }
     return selected_array
+  end
+
+  def my_all?
+    return to_enum(:my_all?) unless block_given?
+
+    all_pass = true
+    self.my_each { |element| all_pass = false unless yield element }
+    return all_pass
+  end
+
+  def my_any?
+    return to_enum(:my_all?) unless block_given?
+
+    self.my_each { |element| return true if yield element }
+
+    return false
+  end
+
+  def my_none?
+    return to_enum(:my_none?) unless block_given?
+
+    self.my_each { |element| return false if yield element }
+
+    return true
   end
 end
 
